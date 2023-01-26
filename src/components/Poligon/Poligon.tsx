f1659a3ef61c20542a4a02e4905f941afe8a856b
Andrey <dovban.andreyy@rambler.ru>
2023-01-27 00:10:57 +0300

    create 625 cells

--- src/components/Poligon/Poligon.tsx
+++ src/components/Poligon/Poligon.tsx
@@ -4,7 +4,7 @@ import Cell from '../Cell/Cell';
 export default function Poligon(): JSX.Element {
     // const arr = [<Cell />, <Cell />, <Cell />].fill(<Cell />, 1);
     let arr: any[] = [];
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 625; i++) {
         arr.push(<Cell />);
     }
     return (
